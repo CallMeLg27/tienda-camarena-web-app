2882d8f729be0934316db0ab55941d00ed929c3d
Luis German Esquivel Grados <lgeldragondorado@gmail.com>
2020-07-20 01:16:00 -0500

    deployment test 1

--- config/config.php
+++ config/config.php
@@ -1,11 +1,19 @@
 <?php
 
-define('URL', 'http://localhost/miProyecto/');
+// --local
+// define('URL', 'http://localhost/miProyecto/');
+// define('HOST', 'localhost');
+// define('DB', 'mvc');
+// define('USER', 'root');
+// define('PASSWORD', "");
+// define('CHARSET', 'utf8mb4');
 
-define('HOST', 'localhost');
-define('DB', 'mvc');
-define('USER', 'root');
-define('PASSWORD', "");
+// --deployed
+define('URL', 'https://mysql2727.herokuapp.com/');
+define('HOST', 'us-cdbr-east-02.cleardb.com');
+define('DB', 'heroku_1401742a30af2c1');
+define('USER', 'b078eb90b93752');
+define('PASSWORD', "92da2499");
 define('CHARSET', 'utf8mb4');
 
 ?>
