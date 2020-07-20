e8d053dccadad5cc4b6ed42d32f5be6e32d046a4
Luis German Esquivel Grados <lgeldragondorado@gmail.com>
2020-07-20 01:00:58 -0500

    bug fixes

--- views/header.php
+++ views/header.php
@@ -11,7 +11,7 @@
 <div id="header">
 <ul>
     <li><a href="<?php echo constant('URL'); ?>index">Inicio</a></li>
-    <li><a href="<?php echo constant('URL'); ?>producto">Producto</a></li>
+    <li><a href="<?php echo constant('URL'); ?>crudproducto">Producto</a></li>
     <li><a href="<?php echo constant('URL'); ?>pedido">Pedido</a></li>
 </ul>
 
