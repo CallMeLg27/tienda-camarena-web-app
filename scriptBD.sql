486b60d40b43b468420a13dcb9a7d17afcc72afb
Luis German Esquivel Grados <lgeldragondorado@gmail.com>
2020-09-24 06:56:26 -0500

    se cargo el script de bd

--- scriptBD.sql
+++ scriptBD.sql
@@ -11,6 +11,11 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Volcando estructura de base de datos para heroku_ba968b3eda52761
+CREATE DATABASE IF NOT EXISTS `heroku_ba968b3eda52761` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `heroku_ba968b3eda52761`;
+
 -- Volcando estructura para tabla heroku_ba968b3eda52761.cliente
 CREATE TABLE IF NOT EXISTS `cliente` (
   `cliente_id` char(5) NOT NULL,
@@ -28,12 +33,11 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla heroku_ba968b3eda52761.cliente: ~11 rows (aproximadamente)
-DELETE FROM `cliente`;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` (`cliente_id`, `nombre`, `apellido`, `dni`, `telefono`, `edad`, `email`, `distrito`, `direccion`, `estado`) VALUES
-	('CL001', 'Marco', 'Huari', '75897931', '972082374', 18, 'marquitos1213@hotmail.com', 'Santa Anita', 'Calle Los Olmos 123', 'activo'),
-	('CL002', 'Andrea', 'Mendoza', '75897932', '915764235', 20, 'marquitos1213@hotmail.com', 'Ate', 'Calle Los Olmos 123', 'activo'),
-	('CL003', 'Nikol', 'Lopez', '75897933', '987456321', 23, 'marquitos1213@hotmail.com', 'Los Olivos', 'Calle Los Olmos 123', 'activo'),
+INSERT IGNORE INTO `cliente` (`cliente_id`, `nombre`, `apellido`, `dni`, `telefono`, `edad`, `email`, `distrito`, `direccion`, `estado`) VALUES
+	('CL001', 'Alejandro', 'Gonzales', '75897931', '972082374', 18, 'marquitos1213@hotmail.com', 'Santa Anita', 'Calle Los Olmos 123', 'activo'),
+	('CL002', 'Andrea', 'Saavedra', '75897932', '915764235', 20, 'marquitos1213@hotmail.com', 'Ate', 'Calle Los Olmos 123', 'activo'),
+	('CL003', 'Kike', 'Quispe', '75897933', '987456321', 23, 'marquitos1213@hotmail.com', 'Los Olivos', 'Calle Los Olmos 123', 'activo'),
 	('CL004', 'Ronny', 'Mallma', '75897934', '972082374', 32, 'marquitos1213@hotmail.com', 'Puente Piedra', 'Calle Los Olmos 123', 'activo'),
 	('CL005', 'Oscar', 'Huayllas', '75897935', '972082374', 56, 'marquitos1213@hotmail.com', 'Santa Anita', 'Calle Los Olmos 123', 'activo'),
 	('CL006', 'Rodolfo', 'Vega', '75897936', '972082374', 70, 'marquitos1213@hotmail.com', 'La Molina', 'Calle Los Olmos 123', 'activo'),
@@ -41,7 +45,7 @@ INSERT INTO `cliente` (`cliente_id`, `nombre`, `apellido`, `dni`, `telefono`, `e
 	('CL008', 'Luis', 'Huaman', '75897938', '972082374', 34, 'marquitos1213@hotmail.com', 'Santa Anita', 'Calle Los Olmos 123', 'activo'),
 	('CL009', 'Pancracio', 'Romero', '75897939', '972082374', 64, 'marquitos1213@hotmail.com', 'Los Olivos', 'Calle Los Olmos 123', 'activo'),
 	('CL010', 'Anastacia', 'Ruiz', '75897911', '972082374', 28, 'marquitos1213@hotmail.com', 'Ate', 'Calle Las Tinieblas 1123', 'activo'),
-	('CL011', 'prueba', 'prueba', '74716688', '999999999', 21, 'nprueba@homail.com', 'san borja', 'Calle Los Olivos 433', 'inactivo');
+	('CL011', 'Kevin', 'Kuroki', '74716688', '996887199', 21, 'nprueba@homail.com', 'san borja', 'Calle Los Olivos 433', 'inactivo');
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 
 -- Volcando estructura para tabla heroku_ba968b3eda52761.comprobantepago
@@ -55,9 +59,8 @@ CREATE TABLE IF NOT EXISTS `comprobantepago` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla heroku_ba968b3eda52761.comprobantepago: ~10 rows (aproximadamente)
-DELETE FROM `comprobantepago`;
 /*!40000 ALTER TABLE `comprobantepago` DISABLE KEYS */;
-INSERT INTO `comprobantepago` (`comprobantepago_id`, `venta_id`, `fecha`) VALUES
+INSERT IGNORE INTO `comprobantepago` (`comprobantepago_id`, `venta_id`, `fecha`) VALUES
 	('CP001', 'VE001', '2002-10-19'),
 	('CP002', 'VE002', '2002-10-19'),
 	('CP003', 'VE003', '2002-10-19'),
@@ -81,9 +84,8 @@ CREATE TABLE IF NOT EXISTS `detalle_promocion_producto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla heroku_ba968b3eda52761.detalle_promocion_producto: ~10 rows (aproximadamente)
-DELETE FROM `detalle_promocion_producto`;
 /*!40000 ALTER TABLE `detalle_promocion_producto` DISABLE KEYS */;
-INSERT INTO `detalle_promocion_producto` (`producto_id`, `promocion_id`) VALUES
+INSERT IGNORE INTO `detalle_promocion_producto` (`producto_id`, `promocion_id`) VALUES
 	('PR001', 'PM001'),
 	('PR002', 'PM002'),
 	('PR003', 'PM003'),
@@ -111,15 +113,14 @@ CREATE TABLE IF NOT EXISTS `empleado` (
   PRIMARY KEY (`empleado_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla heroku_ba968b3eda52761.empleado: ~4 rows (aproximadamente)
-DELETE FROM `empleado`;
+-- Volcando datos para la tabla heroku_ba968b3eda52761.empleado: ~5 rows (aproximadamente)
 /*!40000 ALTER TABLE `empleado` DISABLE KEYS */;
-INSERT INTO `empleado` (`empleado_id`, `nombre`, `dni`, `telefono`, `email`, `sueldo`, `direccion`, `id`, `contraseña`, `rol`) VALUES
-	('E0001', 'Libardo', 'Lara', '25487615', 'aaaaa@gmail.com', 2000, 'Calle Los Olmos 123','aaaaa','11111', 'admin'),
-	('E0002', 'Angie', 'Palacios', '84597612', 'bbbbb@gmail.com', 750, 'Calle Los Olmos 123','bbbbb','22222', 'almacenero'),
-	('E0003', 'Linda', 'Vargas', '48721044', 'ccccc@gmail.com', 800, 'Calle Los Olmos 123''ccccc','33333', 'atCliente',),
-	('E0004', 'Yordan', 'Sullca', '75854312', 'ddddd@gmail.com', 800, 'Calle Los Olmos 123','ddddd','44444', 'atCliente'),
-	('E0005', 'Luis', 'Esquivel', '48726523', 'eeeee@gmail.com', 800, 'Calle Los Olmos 123','eeeee','55555', 'atCliente');
+INSERT IGNORE INTO `empleado` (`empleado_id`, `nombre`, `dni`, `telefono`, `email`, `sueldo`, `direccion`, `id`, `contraseña`, `rol`) VALUES
+	('E0001', 'Reinaldo', 'House', '25487615', 'aaaaa@gmail.com', 2000.00, 'Calle Los Olmos 123', 'aaaaa', '11111', 'admin'),
+	('E0002', 'Angie', 'Palacios', '84597612', 'bbbbb@gmail.com', 750.00, 'Calle Los Olmos 123', 'bbbbb', '22222', 'almacenero'),
+	('E0003', 'Linda', 'Vargas', '48721044', 'ccccc@gmail.com', 800.00, 'Calle Los Olmos 123', 'ccccc', '33333', 'atCliente'),
+	('E0004', 'Yordan', 'Sullca', '75854312', 'ddddd@gmail.com', 800.00, 'Calle Los Olmos 123', 'ddddd', '44444', 'atCliente'),
+	('E0005', 'Luis', 'Esquivel', '48726523', 'eeeee@gmail.com', 800.00, 'Calle Los Olmos 123', 'eeeee', '55555', 'atCliente');
 /*!40000 ALTER TABLE `empleado` ENABLE KEYS */;
 
 -- Volcando estructura para tabla heroku_ba968b3eda52761.pedido
@@ -131,20 +132,25 @@ CREATE TABLE IF NOT EXISTS `pedido` (
   CONSTRAINT `FK_PEDIDO_CLIENTE` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`cliente_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla heroku_ba968b3eda52761.pedido: ~10 rows (aproximadamente)
-DELETE FROM `pedido`;
+-- Volcando datos para la tabla heroku_ba968b3eda52761.pedido: ~16 rows (aproximadamente)
 /*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
-INSERT INTO `pedido` (`pedido_id`, `cliente_id`) VALUES
+INSERT IGNORE INTO `pedido` (`pedido_id`, `cliente_id`) VALUES
+	('PE015', NULL),
 	('PE001', 'CL001'),
 	('PE002', 'CL001'),
 	('PE003', 'CL001'),
 	('PE004', 'CL002'),
 	('PE005', 'CL002'),
+	('PE011', 'CL002'),
+	('PE012', 'CL002'),
 	('PE006', 'CL003'),
 	('PE007', 'CL003'),
 	('PE008', 'CL003'),
 	('PE009', 'CL003'),
-	('PE010', 'CL004');
+	('PE010', 'CL004'),
+	('PE013', 'CL004'),
+	('PE014', 'CL004'),
+	('PE016', 'CL004');
 /*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
 
 -- Volcando estructura para tabla heroku_ba968b3eda52761.producto
@@ -158,9 +164,8 @@ CREATE TABLE IF NOT EXISTS `producto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla heroku_ba968b3eda52761.producto: ~10 rows (aproximadamente)
-DELETE FROM `producto`;
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
-INSERT INTO `producto` (`producto_id`, `nombre`, `descripcion`, `cantidad`, `costo`) VALUES
+INSERT IGNORE INTO `producto` (`producto_id`, `nombre`, `descripcion`, `cantidad`, `costo`) VALUES
 	('PR001', 'LECHE', 'LECHE DE VACA SALUDABLE', 50, 3.00),
 	('PR002', 'MANTEQUILLA', 'SUEVA COMO LA MANTY', 50, 4.00),
 	('PR003', 'PAPEL', 'SEDOSO', 50, 1.00),
@@ -184,16 +189,18 @@ CREATE TABLE IF NOT EXISTS `productopedido` (
   CONSTRAINT `FK_PRODUCTOPEDIDO_PRODUCTO` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`producto_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla heroku_ba968b3eda52761.productopedido: ~10 rows (aproximadamente)
-DELETE FROM `productopedido`;
+-- Volcando datos para la tabla heroku_ba968b3eda52761.productopedido: ~13 rows (aproximadamente)
 /*!40000 ALTER TABLE `productopedido` DISABLE KEYS */;
-INSERT INTO `productopedido` (`producto_id`, `pedido_id`, `cantidad`) VALUES
+INSERT IGNORE INTO `productopedido` (`producto_id`, `pedido_id`, `cantidad`) VALUES
 	('PR001', 'PE001', 3),
+	('PR001', 'PE011', 7),
 	('PR002', 'PE002', 3),
+	('PR002', 'PE011', 20),
 	('PR003', 'PE003', 1),
 	('PR004', 'PE004', 6),
 	('PR005', 'PE005', 2),
 	('PR006', 'PE006', 3),
+	('PR006', 'PE012', 3),
 	('PR007', 'PE007', 2),
 	('PR008', 'PE008', 4),
 	('PR009', 'PE009', 2),
@@ -214,9 +221,8 @@ CREATE TABLE IF NOT EXISTS `promocion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla heroku_ba968b3eda52761.promocion: ~10 rows (aproximadamente)
-DELETE FROM `promocion`;
 /*!40000 ALTER TABLE `promocion` DISABLE KEYS */;
-INSERT INTO `promocion` (`promocion_id`, `nombre`, `descripcion`, `cantidad`, `producto_id`, `costo`) VALUES
+INSERT IGNORE INTO `promocion` (`promocion_id`, `nombre`, `descripcion`, `cantidad`, `producto_id`, `costo`) VALUES
 	('PM001', '2X1 LECHE', 'LLEVATE DOS LECHES POR EL PRECIO DE UNO', 10, NULL, NULL),
 	('PM002', '2X1 MANTEQUILLA', 'LLEVATE DOS MANTEQUILLAS POR EL PRECIO DE UNO', 10, NULL, NULL),
 	('PM003', '2X1 PAPEL', 'LLEVATE DOS PAPELES POR EL PRECIO DE UNO', 10, NULL, NULL),
@@ -240,9 +246,8 @@ CREATE TABLE IF NOT EXISTS `recarga` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla heroku_ba968b3eda52761.recarga: ~12 rows (aproximadamente)
-DELETE FROM `recarga`;
 /*!40000 ALTER TABLE `recarga` DISABLE KEYS */;
-INSERT INTO `recarga` (`recarga_id`, `tarjeta_id`, `monto`) VALUES
+INSERT IGNORE INTO `recarga` (`recarga_id`, `tarjeta_id`, `monto`) VALUES
 	('RE001', 'TJ001', 100.00),
 	('RE002', 'TJ001', 50.00),
 	('RE003', 'TJ001', 120.00),
@@ -269,11 +274,10 @@ CREATE TABLE IF NOT EXISTS `tarjeta` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla heroku_ba968b3eda52761.tarjeta: ~10 rows (aproximadamente)
-DELETE FROM `tarjeta`;
 /*!40000 ALTER TABLE `tarjeta` DISABLE KEYS */;
-INSERT INTO `tarjeta` (`tarjeta_id`, `cliente_id`, `fechavencimiento`, `saldo`) VALUES
+INSERT IGNORE INTO `tarjeta` (`tarjeta_id`, `cliente_id`, `fechavencimiento`, `saldo`) VALUES
 	('TJ001', 'CL001', '2001-09-20', 55.00),
-	('TJ002', 'CL002', '2001-09-20', 25.00),
+	('TJ002', 'CL002', '2001-09-20', 16.00),
 	('TJ003', 'CL003', '2001-09-20', 104.00),
 	('TJ004', 'CL004', '2001-09-20', 40.00),
 	('TJ005', 'CL005', '2001-09-20', 35.00),
@@ -295,9 +299,8 @@ CREATE TABLE IF NOT EXISTS `ticket` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla heroku_ba968b3eda52761.ticket: ~10 rows (aproximadamente)
-DELETE FROM `ticket`;
 /*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
-INSERT INTO `ticket` (`ticket_id`, `recarga_id`, `fecha`) VALUES
+INSERT IGNORE INTO `ticket` (`ticket_id`, `recarga_id`, `fecha`) VALUES
 	('TK001', 'RE001', '2001-09-19'),
 	('TK002', 'RE002', '2003-09-19'),
 	('TK003', 'RE003', '2005-09-19'),
@@ -322,10 +325,10 @@ CREATE TABLE IF NOT EXISTS `venta` (
   CONSTRAINT `FK_VENTA_PEDIDO` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`pedido_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla heroku_ba968b3eda52761.venta: ~10 rows (aproximadamente)
-DELETE FROM `venta`;
+-- Volcando datos para la tabla heroku_ba968b3eda52761.venta: ~12 rows (aproximadamente)
 /*!40000 ALTER TABLE `venta` DISABLE KEYS */;
-INSERT INTO `venta` (`venta_id`, `pedido_id`, `descripcion`, `monto`, `fecha`) VALUES
+INSERT IGNORE INTO `venta` (`venta_id`, `pedido_id`, `descripcion`, `monto`, `fecha`) VALUES
+	('VE', NULL, NULL, 0, '2020-09-17'),
 	('VE001', 'PE001', 'COMPRA DE ABARROTES - MINIMARKET CAMARENA', 15, '2002-10-19'),
 	('VE002', 'PE002', 'COMPRA DE ABARROTES - MINIMARKET CAMARENA', 20, '2002-10-19'),
 	('VE003', 'PE003', 'COMPRA DE ABARROTES - MINIMARKET CAMARENA', 22, '2002-10-19'),
@@ -335,7 +338,8 @@ INSERT INTO `venta` (`venta_id`, `pedido_id`, `descripcion`, `monto`, `fecha`) V
 	('VE007', 'PE007', 'COMPRA DE ABARROTES - MINIMARKET CAMARENA', 50, '2004-10-19'),
 	('VE008', 'PE008', 'COMPRA DE ABARROTES - MINIMARKET CAMARENA', 35, '2004-10-19'),
 	('VE009', 'PE009', 'COMPRA DE ABARROTES - MINIMARKET CAMARENA', 21, '2004-10-19'),
-	('VE010', 'PE010', 'COMPRA DE ABARROTES - MINIMARKET CAMARENA', 18, '2005-10-19');
+	('VE010', 'PE010', 'COMPRA DE ABARROTES - MINIMARKET CAMARENA', 18, '2005-10-19'),
+	('VE012', 'PE012', NULL, 9, '2020-09-17');
 /*!40000 ALTER TABLE `venta` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
