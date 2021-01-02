dbd4dfcbb1902588a1109b0970ab96d2fa978dff
Luis German Esquivel Grados <lgeldragondorado@gmail.com>
2021-01-01 22:23:22 -0500

    awatafaco

--- config/config.php
+++ config/config.php
@@ -10,19 +10,20 @@
  
 // --deployed
 if (isset($_ENV["DB_HOST"])){
+    echo("YES");
     define('URL', 'https://tienda-camarena-new.herokuapp.com/');
     define('HOST', $_ENV["DB_HOST"]);
     define('DB', $_ENV["DB"]);
     define('USER', $_ENV["DB_USER"]);
     define('PASSWORD', $_ENV["DB_PASSWORD"]);
 }else{
+    echo("NO");
     define('URL', 'http://localhost/tienda-camarena-web-app/');
     define('HOST', "localhost");
     define('DB', "tienda_camarena");
     define('USER', "root");
     define('PASSWORD', "");
 }
-echo isset($_ENV["DB_HOST"]);
 define('CHARSET', 'utf8mb4');
  ?>
 <script>
