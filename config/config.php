6824b8c2332e744c7540e455f39caa0ca06ab933
Luis German Esquivel Grados <lgeldragondorado@gmail.com>
2021-01-01 22:27:36 -0500

    se cambió el servidor de base de datos

--- config/config.php
+++ config/config.php
@@ -10,14 +10,12 @@
  
 // --deployed
 if (isset($_ENV["DB_HOST"])){
-    echo("YES");
     define('URL', 'https://tienda-camarena-new.herokuapp.com/');
     define('HOST', $_ENV["DB_HOST"]);
     define('DB', $_ENV["DB"]);
     define('USER', $_ENV["DB_USER"]);
     define('PASSWORD', $_ENV["DB_PASSWORD"]);
 }else{
-    echo("NO");
     define('URL', 'http://localhost/tienda-camarena-web-app/');
     define('HOST', "localhost");
     define('DB', "tienda_camarena");
